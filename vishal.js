c4fe3554b5b7f041c0930a5f9d9db8d53fdcb82d
Vishal17599 <Vishal.parmar17599@gmail.com>
2021-04-07 23:16:36 +0530

    changes

--- vishal.js
+++ vishal.js
@@ -36,6 +36,11 @@ function input() {
     document.getElementById("main").style.display = "block";
 
     document.getElementById("ques").innerHTML = rootques;
+    var ul = document.getElementById("log");
+    var candidate = rootques;
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(candidate));
+    ul.appendChild(li);
 }
 
 
@@ -43,18 +48,67 @@ var level = 1;
 var para = 4;
 
 
+function notSure() {
+    var j = -1, f = 0, t = 0;
+    // para++;
+    for (let i = 4; i < obj[level].length; i++) {
+        if (obj[level][i][0] == para.toString() && f == 0) {
+            f = i;
+        }
+        else if (obj[level][i][0] == para.toString() && f != 0) {
+            j = i;
+            break;
+        }
+    }
+    if (obj[level][f][obj[level][f].length - 1] == "?") {
+        stack.push(level);
+        stack.push(f);
+        document.getElementById("ques").innerHTML = obj[level][f].substring(1, obj[level][f].length);
+        level++;
+        t = 1;
+    }
+    else {
+        //Add it to Log(This is the statement without question mark)
+    }
+    if (obj[level][j][obj[level][j].length - 1] == "?" && t == 0) {
+        stack.push(level);
+        stack.push(j);
+        document.getElementById("ques").innerHTML = obj[level][j].substring(1, obj[level][j].length);
+        level++;
+    }
+    else {
+        //Add it to Log(This is the statement without question mark)
+    }
+}
+
+
+
+
+
+
 function undo() {
-    if (stack.length > 0) {
+    if (stack.length > 2) {
         let b = stack.pop();
         let a = stack.pop();
         b = stack.pop();
         a = stack.pop();
         console.log(a);
         console.log(b);
+        console.log(obj[a][b].substring(1, obj[a][b].length));
+        document.getElementById("res").style.display = "none";
+        document.getElementById("ques").style.display = "block";
+        document.getElementById("ans1").style.display = "inline-block";
+        document.getElementById("ans2").style.display = "inline-block";
+        document.getElementById("ans3").style.display = "none";
         document.getElementById("ques").innerHTML = obj[a][b].substring(1, obj[a][b].length);
         stack.push(a);
         stack.push(b);
         level = a + 1;
+
+        var list = document.getElementById("log");
+        list.removeChild(list.lastChild);
+        list.removeChild(list.lastChild);
+
     }
 }
 
@@ -66,6 +120,14 @@ function next() {
     else {
         stack.push(level);
         stack.push(para);
+
+
+        var ul = document.getElementById("log");
+        var candidate = obj[level][para].substring(1, obj[level][para].length);
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(candidate));
+        ul.appendChild(li);
+
         document.getElementById("res").style.display = "none";
         document.getElementById("ques").style.display = "block";
         document.getElementById("ques").innerHTML = obj[level][para].substring(1, obj[level][para].length);
@@ -80,6 +142,12 @@ function next() {
 
 
 function vishalyes() {
+    var ul = document.getElementById("log");
+    var candidate = "Yes";
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(candidate));
+    ul.appendChild(li);
+
     // document.getElementById("ans3").style.display = "none";
     // console.log(obj[level][para]);
     // if (para != 4)
@@ -102,21 +170,37 @@ function vishalyes() {
         if (obj[level][j][obj[level][j].length - 1] == "?") {
             // console.log(obj[level][j]);
             document.getElementById("ques").innerHTML = obj[level][j].substring(1, obj[level][j].length);
+            var candidate = obj[level][j].substring(1, obj[level][j].length);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(candidate));
+            ul.appendChild(li);
             level++;
         }
         else {
             document.getElementById("ques").style.display = "none";
             document.getElementById("ans1").style.display = "none";
             document.getElementById("ans2").style.display = "none";
-            document.getElementById("ans3").style.display = "block";
+            document.getElementById("ans3").style.display = "inline-block";
             document.getElementById("res").style.display = "block";
             // console.log(obj[level][j]);
             document.getElementById("res").innerHTML = obj[level][j].substring(1, obj[level][j].length);
+            var candidate = obj[level][j].substring(1, obj[level][j].length);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(candidate));
+            ul.appendChild(li);
             para++;
         }
     }
 }
 function vishalNo() {
+
+    var ul = document.getElementById("log");
+
+    var candidate = "No";
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(candidate));
+    ul.appendChild(li);
+
     // if (para != 4)
     //     para--;
     var j = -1, f = 0;
@@ -141,15 +225,24 @@ function vishalNo() {
         if (obj[level][j][obj[level][j].length - 1] == "?") {
             // console.log(obj[level][j]);
             document.getElementById("ques").innerHTML = obj[level][j].substring(1, obj[level][j].length);
+            var candidate = obj[level][j].substring(1, obj[level][j].length);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(candidate));
+            ul.appendChild(li);
             level++;
         }
         else {
             document.getElementById("ques").style.display = "none";
             document.getElementById("ans1").style.display = "none";
             document.getElementById("ans2").style.display = "none";
-            document.getElementById("ans3").style.display = "block";
+            document.getElementById("ans3").style.display = "inline-block";
             document.getElementById("res").style.display = "block";
             document.getElementById("res").innerHTML = obj[level][j].substring(1, obj[level][j].length);
+
+            var candidate = obj[level][j].substring(1, obj[level][j].length);
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(candidate));
+            ul.appendChild(li);
             para++;
         }
     }
